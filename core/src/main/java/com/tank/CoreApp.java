3026aea2da8a6659bdd9dd1df9ce966aa38ea089
Tank <fuch@fullsample.com>
2018-10-05 21:46:18 +0800

    [fuchun] #Tech rename

--- core/src/main/java/com/tank/CoreApp.java
+++ core/src/main/java/com/tank/CoreApp.java
@@ -10,8 +10,8 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class App {
+public class CoreApp {
   public static void main(String[] args) {
-    SpringApplication.run(App.class);
+    SpringApplication.run(CoreApp.class);
   }
 }
